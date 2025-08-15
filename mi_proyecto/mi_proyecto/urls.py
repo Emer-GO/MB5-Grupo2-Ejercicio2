54949060712104011969627a6ce3efca05a8a5f3
er58933@gmail.com <er58933@gmail.com>
2025-08-15 11:07:01 -0500

    Proyecto prueba

--- mi_proyecto/mi_proyecto/urls.py
+++ mi_proyecto/mi_proyecto/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MI_BANCO import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.inicio),
 ]
